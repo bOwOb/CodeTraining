19e132df70852e4d56987c9f8861707bed10763c
kulee <kangwook1530@gmail.com>
2022-11-01 01:15:31 +0900

    add rpg

--- C,C++/rpgGame/rpgGame/rpg.cpp
+++ C,C++/rpgGame/rpgGame/rpg.cpp
@@ -7,9 +7,9 @@ typedef unsigned int uint;
 
 enum ctype {
 	WARRIOR = 00,
-	WIZARD = 01,
-	ARCHER = 10,
-	PRIEST = 11
+	WIZARD  = 01,
+	ARCHER  = 10,
+	PRIEST  = 11
 };
 
 typedef struct {
@@ -23,7 +23,7 @@ typedef struct {
 	};
 
 	union {
-		uint level;
+		uint level;	
 	};
 
 	union {
@@ -33,42 +33,76 @@ typedef struct {
 		};
 	};
 
-}character;
-//int setCharInfo(character* _char) {
-//	uint type;
-//
-//
-//	return 0;
-//}
+	union {
+		uint abil;
+		uint st;
+		uint mp;
+		uint ar;
+		uint pr;
+	};
+
+}character ;
+int setCharInfo(character* _char) {
+	uint type;
+
+	printf("select type: 0.Warrior 1.Wizard 2.Archer 3.Priest : ");
+	scanf("%d", &type);
+
+	_char->id = 1000;
+	if (type == 0) {
+		_char->idType = WARRIOR;
+	}
+
+	printf("Enter [name] [hp] [st|mp|ar|pr] : ");
+	scanf("%s %d %d", &_char->name, &_char->hp, &_char->abil);
+
+	return 0;
+}
 
 
 int levelUp(character* _char) {
 	_char->level++;
 	_char->hp = _char->hp * 1.1;
+	_char->abil = _char->abil*1.1;
 
 	return 0;
 }
 
-//void printCharInfo(character* _char) {
-//	//uint type;
-//
-//
-//	printf("[Name] %s [ID] %u [%s, Lv.%d] [HP] %u", _char->name, _char->id, _char->idType, _char->level, _char->hp);
-//
-//	printf("%u \n", _char->abil);
-//}
+void printCharInfo(character* _char) {
+	//uint type;
+
+	
+	printf("[Name] %s [ID] %u [%s, Lv.%d] [HP] %u", _char->name, _char->id, _char->idType, _char->level, _char->hp);
+
+	if(_char->idType == 00){
+		printf("[ST]");
+	}
+	else if (_char->idType == 01) {
+		printf("[MP]");
+	}
+	else if (_char->idType == 10) {
+		printf("[AR]");
+	}
+	else if (_char->idType == 11) {
+		printf("[PR]");
+	}
+	else {
+
+	}
+		printf("%u \n", _char->abil);
+}
 
 
 int main() {
 	int i;
-	character user = { 0 };//error
+	character user = {0};//error
 
-	//setCharInfo(&user);
-//	printCharInfo(&user);
+	setCharInfo(&user);
+	printCharInfo(& user);
 	for (i = 0; i < 5; i++) {
 		levelUp(&user);
-		//printCharInfo(&user);
+		printCharInfo(&user);
 	}
-	return 0;
+		return 0;
 }
 //ref . https://dojang.io/mod/page/view.php?id=454
